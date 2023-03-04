89eba75277c108f7f285e096bda5edab55f758ae
OlegWock <olegwock@gmail.com>
2023-03-04 18:08:00 +0100

    Remove tls bypass

--- src/htmlshare_api.py
+++ src/htmlshare_api.py
@@ -4,7 +4,7 @@ from httpx import AsyncClient
 from utils import get_logger
 
 
-HTMLSHARE_ROOT_URL = os.getenv('HTMLSHARE_BASE_URL', 'https://raindrop-html-share.sinja.io')
+HTMLSHARE_ROOT_URL = os.getenv('HTMLSHARE_BASE_URL', 'https://raindropio-html-share.sinja.io/')
 HTMLSHARE_PASSWORD = os.getenv('HTMLSHARE_PASSWORD', '')
 
 logger = get_logger('bot')
@@ -12,9 +12,7 @@ logger = get_logger('bot')
 logger.info(f"Using {HTMLSHARE_ROOT_URL} as htmlshare domain")
 
 async def upload_html(html: str):
-    # TODO: need to figure out why this fails `httpcore.ConnectError: [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: self signed certificate`
-    # if verify=True (or ommited) when calling htmlshare on sinja.io
-    async with AsyncClient(verify=False) as client:
+    async with AsyncClient() as client:
         response = await client.post(f'{HTMLSHARE_ROOT_URL}/html', json={"html": html, "password": HTMLSHARE_PASSWORD})
         if response.status_code != 200:
             return None
